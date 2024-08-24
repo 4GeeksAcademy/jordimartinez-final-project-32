518f782b6ffe73f1ee0dca62e2d6a3923df53bdb
Jordi Guiusseppe Martinez Valencia <166647357+JordiBeppe@users.noreply.github.com>
2024-08-24 02:42:24 +0000

    carousel added

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,23 +7,54 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
 
-			<h1>Hola, Que tal?</h1>
+		<>
 
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+			<div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+				<div className="carousel-indicators">
+					<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+					<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+					<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+				</div>
+				<div className="carousel-inner">
+					<div className="carousel-item active">
+					<img src="https://picsum.photos//900/300" className="d-block w-100" alt="..."/>
+					</div>
+					<div className="carousel-item">
+					<img src="https://picsum.photos//600/200" className="d-block w-100" alt="..."/>
+					</div>
+					<div className="carousel-item">
+					<img src="https://picsum.photos//450/150" className="d-block w-100" alt="..."/>
+					</div>
+				</div>
+				<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+					<span className="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span className="visually-hidden">Previous</span>
+				</button>
+				<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+					<span className="carousel-control-next-icon" aria-hidden="true"></span>
+					<span className="visually-hidden">Next</span>
+				</button>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
+
+			<div className="text-center mt-5">
+				<h1>Hello Rigo!!</h1>
+
+				<h1>Hola, Que tal?</h1>
+
+				<p>
+					<img src={rigoImageUrl} />
+				</p>
+				<div className="alert alert-info">
+					{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+				</div>
+				<p>
+					This boilerplate comes with lots of documentation:{" "}
+					<a href="https://start.4geeksacademy.com/starters/react-flask">
+						Read documentation
+					</a>
+				</p>
+			</div>
+		</>
 	);
 };
