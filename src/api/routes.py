261bf3ad175688542126543fd6271f6f2fe51800
Daniel Perdomo <136826084+GoblinSwarm@users.noreply.github.com>
2024-08-29 01:35:27 +0000

    Fixed a problem in models.py, added CRD category routes in routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,11 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Category
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+
+
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +22,46 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/category', methods=['GET'])
+def get_categories():
+    categories = Category()
+    categories = categories.query.all()
+    return jsonify([item.serialize() for item in categories]), 200
+
+@api.route('/category/<int:theid>', methods=['GET'])
+def get_one_category(theid=None):
+    if theid is not None:
+        category = Category()
+        category = category.query.get(theid)
+        if category is not None:
+            return jsonify(category.serialize()), 200
+        else:
+            return jsonify({"message": "Category not found"}), 404
+    return jsonify({"message": "Id is None"}), 400
+
+@api.route('/category', methods=['POST'])
+def add_category():
+    
+    data = request.json
+    if data is not None:
+        category = Category(name=data['name'])
+        db.session.add(category)
+        db.session.commit()
+        return jsonify({"message": "Adding a Category"}), 201
+    else:
+        return jsonify({"message": "Couldnt add category"}), 400
+    
+@api.route('/category/<int:theid>', methods=['DELETE'])
+def delete_category(theid):
+    
+    if theid is not None:
+        category = Category()
+        category = category.query.get(theid)
+        if category is not None:
+            db.session.delete(category)
+            db.session.commit()
+            return jsonify({"message": "Category Deleted"}), 200
+        else:
+            return jsonify({"message": "Category not found"}), 404
+
